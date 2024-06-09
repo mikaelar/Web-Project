f5273db8d2543736fa86059a4437c7ad1f4a5573
Mikaela Ezekieva <mikaela.ezekieva@sap.com>
2024-06-09 15:26:39 +0300

    add notifier

--- app/create_project/create_project.php
+++ app/create_project/create_project.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL);
 
 require_once '../Database.php';
 require_once '../Project.php';
+require_once '../Notifier.php';
 
 $servername = "localhost";
 $dbusername = "root";
@@ -13,8 +14,14 @@ $dbname = "form";
 $db = new Database($servername, $dbusername, $dbpassword, $dbname);
 
 $project = new Project($db);
+$notifier = new Notifier($db);
 
 $project->setProjectDetails($_POST['name'], $_POST['description'], $_POST['collaborators'], $_POST['initial_requirements']);
 
-$project->create();
+if ($project->create()) {
+    $notifier->addNotification("Нов проект е създаден: " . $_POST['name']);
+    header("Location: ../manage_homepage/homepage.php");
+} else {
+    header("Location: ../create_project/create_project.html");
+}
 ?>
